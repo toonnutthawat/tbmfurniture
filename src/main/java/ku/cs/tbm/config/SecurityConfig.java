bacd0293f7d43e3da43899b5cd06cc3abb9081b2
Nutthawat Aonprasert <nutthawat.ao@ku.th>
2023-10-08 23:42:04 +0700

    first commit

--- src/main/java/ku/cs/tbm/config/SecurityConfig.java
+++ src/main/java/ku/cs/tbm/config/SecurityConfig.java
@@ -1,15 +1,23 @@
 package ku.cs.tbm.config;
 
+import ku.cs.tbm.service.UserDetailsServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    private UserDetailsServiceImp userDetailsService;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
     {
@@ -18,8 +26,32 @@ public class SecurityConfig {
                         .requestMatchers(new AntPathRequestMatcher("/")).permitAll()
                         .requestMatchers(new AntPathRequestMatcher("/css/**")).permitAll()
                         .requestMatchers(new AntPathRequestMatcher("/js/**")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/signup")).permitAll()
                         .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/", true)
+                        .permitAll()
+                )
+                .logout((logout) -> logout
+                        .logoutUrl("/logout")
+                        .clearAuthentication(true)
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID", "remember-me")
+                        .permitAll()
                 );
+
         return http.build();
     }
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
+    }
+
 }
---
+++ src/main/java/ku/cs/tbm/service/SignupService.java
@@ -0,0 +1,32 @@
+package ku.cs.tbm.service;
+
+import ku.cs.tbm.entity.Member;
+import ku.cs.tbm.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SignupService {
+    @Autowired
+    private MemberRepository repository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public boolean isUsernameAvailable(String username) {
+        return repository.findByUsername(username) == null;
+    }
+    public void createUser(Member user) {
+        Member record = new Member();
+        record.setName(user.getName());
+        record.setUsername(user.getUsername());
+        record.setRole(user.getRole());
+        String hashedPassword = passwordEncoder.encode(user.getPassword());
+        record.setPassword(hashedPassword);
+        repository.save(record);
+    }
+    public Member getUser(String username) {
+        return repository.findByUsername(username);
+    }
+
+}
